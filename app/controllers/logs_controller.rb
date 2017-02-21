ab939c90590835c2488eddaf0a4ceafa82b6347f
Tristan Gruener <tristan.gruener@gmail.com>
2017-02-21 14:01:27 -0800

    working

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -1,10 +1,11 @@
 class LogsController < ApplicationController
   before_action :set_log, only: [:show, :edit, :update, :destroy]
+  before_action :get_all_logs, only: [:index, :new]
 
   # GET /logs
   # GET /logs.json
   def index
-    @logs = Log.all
+
   end
 
   # GET /logs/1
@@ -65,6 +66,10 @@ class LogsController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_log
       @log = Log.find(params[:id])
+    end
+
+    def get_all_logs
+      @logs = Log.all
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,8 @@ module BpLog
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    config.time_zone = 'America/Los_Angeles'
+
   end
 end
